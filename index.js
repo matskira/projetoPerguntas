e18c62000e54781663bc77be34502d717641e2af
Matheus Mariano Poda <mpoda@server01.foursys.local>
2020-07-29 12:06:31 -0300

    Estrutura condicional com EJS

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ app.get("/:nome/:lang", (req,res) =>{
     //render por padrão, olha sempre a pasta views (Por isso precisamos colocar os nossos HTML nessa pasta)
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = true;
+    
     res.render("index",{
         nome: nome,
         lang: lang,
         empresa: "Guia do Programador!",
-        inscritos: 8000
+        inscritos: 8000,
+        msg: exibirMsg
     });
 });
 
