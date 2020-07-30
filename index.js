2f4441049639b8ab0be90d3f9f97e0a9c5074ba9
Matheus Mariano Poda <mpoda@server01.foursys.local>
2020-07-30 09:56:01 -0300

    Configurando o express para aceitar arquivos estáticos

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 
 //Estou dizendo para o express, usar o EJS como View Engine
 app.set('view engine', 'ejs');
-
+//Estou dizendo para o express, que a pasta public terá arquivos estáticos (CSS, imagens e etc)
+app.use(express.static('public'));
 
 app.get("/:nome/:lang", (req,res) =>{
     //render por padrão, olha sempre a pasta views (Por isso precisamos colocar os nossos HTML nessa pasta)
